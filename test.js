7b43f40f9539dd1484ca837523b615351a98b94f
Asriel Nguyen <ganyu@segs.eu.org>
2025-01-07 10:22:12 +0000

    change: refactor the tests

--- test.js
+++ test.js
@@ -1,54 +1,36 @@
-const { facebook } = require("another-fb-video-downloader");
+const { facebook } = require('./index.js');
 
-// ? facebook(url, lq = true||false)
-// ? url: string
-// ? lq: boolean (true for low quality) (optional)
+async function runTestCases() {
+    // User link (public video)
+    const userVideoUrl = "https://www.facebook.com/gdprofreefire/videos/3529051220729199";
+    const userResult = await facebook(userVideoUrl);
+    console.log("User link");
+    console.log(userResult);
 
-// Public
-(async () => {
+    // Public video URL
+    const publicVideoUrl = "https://www.facebook.com/watch?v=2040559343071046";
+    const publicResult = await facebook(publicVideoUrl);
     console.log("Public");
-    try {
-        const videoUrl1 = await facebook(
-            "https://www.facebook.com/100008639590618/videos/266077793027023/",
-            false
-        );
-        console.log(videoUrl1);
-    } catch (error) {
-        console.error(error);
-    }
+    console.log(publicResult);
 
-    // Private
-    console.log("Private");
-    try {
-        const videoUrl2 = await facebook(
-            "https://www.facebook.com/61550995123378/videos/3139821322821360",
-            false
-        );
-        console.log(videoUrl2);
-    } catch (error) {
-        console.error(error);
-    }
+    // Private video from a group URL
+    const privateGroupVideoUrl = "https://www.facebook.com/hue.nguyenthihue.1675/videos/1356166348710541/?idorvanity=445598535599027";
+    const privateGroupResult = await facebook(privateGroupVideoUrl);
+    console.log("Private, from a group");
+    console.log(privateGroupResult);
 
-    // Error test
-    console.log("Error test");
-    try {
-        const videoUrl3 = await facebook(
-            "https://www.facebook.com/photo/?fbid=726350166339523&set=gm.916158123339104&idorvanity=880640216890895",
-            false
-        );
-        console.log(videoUrl3);
-    } catch (error) {
-        console.error(error);
-    }
-        // fb.watch
-        console.log("share test");
-        try {
-            const videoUrl4 = await facebook(
-                "https://www.facebook.com/share/v/18c4VaXDbY/",
-                false
-            );
-            console.log(videoUrl4);
-        } catch (error) {
-            console.error(error);
-        }
-})();
+    // Private fb.watch video URL
+    const privateWatchVideoUrl = "https://fb.watch/wYK9mqsjYR/";
+    const privateWatchResult = await facebook(privateWatchVideoUrl);
+    console.log("Private, fb.watch link");
+    console.log(privateWatchResult);
+
+    // Share link public video URL
+    const shareLinkVideoUrl = "https://www.facebook.com/share/v/18c4VaXDbY/";
+    const shareLinkResult = await facebook(shareLinkVideoUrl);
+    console.log("Share link public");
+    console.log(shareLinkResult);
+}
+
+// Run the test cases
+runTestCases();
