d98139b131a1a160c65f0fc0d2f436607a68154b
Asriel Nguyen <ganyu@segs.eu.org>
2025-01-07 10:22:27 +0000

    change: add fb.watch test

--- regex.js
+++ regex.js
@@ -1,13 +1,14 @@
 const regex = /(?:facebook|fb).*?(?:\/videos\/|\/reel|\/watch\?v=|\/share\/v\/)(\w+)/i;
 
-
 const testUrls = [
     "https://www.facebook.com/gdprofreefire/videos/3529051220729199",
     "https://www.facebook.com/watch?v=606204218511964",
     "https://www.facebook.com/share/v/18c4VaXDbY/",
+    "https://fb.watch/wYK9mqsjYR/"
 ];
 
 testUrls.forEach(url => {
+    console.log("Testing:", url);  // For better debugging
     const match = url.match(regex);
     console.log(match ? `Matched ID: ${match[1]}` : "No match");
 });
